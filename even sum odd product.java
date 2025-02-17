22abe8b32618a4727796a9befe809ee4f41dacd4
busa nikhitha <busan@Nikhitha.(none)>
2025-02-17 15:33:22 +0530

    Initial commit

---
+++ even sum odd product.java
@@ -0,0 +1,26 @@
+class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
+        int n=1,sum=0,product=1;
+        while(n<=100)
+        {
+             if(n%2==0)
+             {
+                sum=sum+n;
+                
+             }
+             else
+             {
+                 product=product*n;
+                 
+             }
+             
+            
+        n++;
+        }
+        System.out.println(sum+ " even sum");
+        System.out.println(product+ " odd product");
+    }
+}
+                 
---
+++ evenfactors.java
@@ -0,0 +1,14 @@
+class Evenfact {
+public static void main(String[] args) {
+int num=4,start=1,sum=0;
+while(start<=num)
+{
+if(num%start==0 && start%2==0)
+{
+sum=sum+start;
+}
+start++;
+}
+System.out.println("sum of even factors = " +sum);
+}
+}
